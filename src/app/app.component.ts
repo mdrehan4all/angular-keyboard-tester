215d180cb72c392df919004b9cf77809c3ba8c36
Md Rehan <mdrehan4all@gmail.com>
2022-08-02 22:12:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-keyboard-tester';
+  key: any = 'Press any key';
+  history: string = '';
+
+  disableFunctionKeys(e: KeyboardEvent) {
+    var functionKeys = new Array(9, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123);
+    if (functionKeys.indexOf(e.keyCode) > -1 || functionKeys.indexOf(e.which) > -1) {
+        e.preventDefault();
+    }
+  };
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    this.key = event.key;
+    this.disableFunctionKeys(event);
+    this.history = this.history +' '+ this.key;
+    console.log(this.key);
+  }
+
+  clear(){
+    this.history = '';
+  }
 }
