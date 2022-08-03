061ebba03a77c794f0e9d88fc9570956e85880ad
Md Rehan <mdrehan4all@gmail.com>
2022-08-03 16:16:27 +0530

    bug fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,9 @@ import { HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-keyboard-tester';
-  key: any = 'Press any key';
+  key: any = '';
   history: string = '';
+  pressedkey: string = 'Press any key';
 
   disableFunctionKeys(e: KeyboardEvent) {
     var functionKeys = new Array(9, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123);
@@ -21,8 +22,9 @@ export class AppComponent {
   @HostListener('window:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
     this.key = event.key;
+    this.pressedkey = (this.key != ' ' ? this.key : 'Space');
     this.disableFunctionKeys(event);
-    this.history = this.history +' '+ this.key;
+    this.history = this.history +' '+ this.pressedkey;
     console.log(this.key);
   }
 
